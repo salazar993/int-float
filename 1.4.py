fa70c30e7c605d406c5f2d08e3994a48e1b142c1
salazar993 <maimanovmeder@gmail.com>
2021-05-22 13:10:12 +0600

    +,-,*,/

---
+++ 1.4.py
@@ -0,0 +1,27 @@
+#Problem 3
+
+#1
+a = 3
+b = 13
+print(a + b)
+
+#2
+a = 3
+b = 13
+c = 2
+print((a + b) * 2)
+
+#3
+a = 3
+b = 13
+c = 2
+print((a + b) ** c)
+
+#4
+a = 3
+b = 13
+c = 193432
+d = a + b
+e = d * 2
+f = d ** 2
+print((d + e + f) - c)
