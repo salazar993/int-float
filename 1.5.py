105895d294f62406ce1fcfc6aab422ed53022217
salazar993 <maimanovmeder@gmail.com>
2021-05-22 13:13:54 +0600

    >,<,=

---
+++ 1.5.py
@@ -0,0 +1,34 @@
+#Problem1
+
+a = 4
+b = 3
+c = 5
+print((a > b) < c) 
+
+#Problem2
+
+a = 7
+b = 3
+c = 4.8
+d = a % b
+print(d * c)
+
+#Problem3
+
+a = 5
+b = 5
+c = a + b
+d = 3
+e = 7
+f = d + e
+print(c == f)
+
+#Problem4
+
+a = 5
+b = 5
+c = a + b
+d = 3
+e = 6
+f = d + e
+print(c != f)
